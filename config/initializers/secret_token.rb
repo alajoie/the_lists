a587515990d66072c6c6859d6f4480ea157e9873
Alexis La Joie <alajoie@redpointtech.com>
2012-01-05 22:07:41 -0600

    Added testing frameworks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheLists::Application.config.secret_token = 'd403358b7531a16737da625cf4cc3f87b315e58e332bec431b2d172fde2cb51f55ee2db75ebf473ec75aa7168bb6f9e5bf693da6b39046e55ae219e1cd8767da'
